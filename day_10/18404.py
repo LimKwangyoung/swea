e5ff35d4e9417f603038ec9588f0d0f0d088dbe0
LimKwangyoung <kokoz22@naver.com>
2024-02-13 15:09:48 +0900

    day_10

--- day_10/18404.py
+++ day_10/18404.py
@@ -7,14 +7,15 @@ sys.stdin = open(f'{__file__.split("/")[-1][:-3]}_input.txt')
 def dfs(idx: int, total: int) -> None:
     global result
 
-    if total + idx > 10:
+    total += idx
+    if total > 10:
         return
-    elif total + idx == 10:
+    elif total == 10:
         result += 1
         return
 
     for i in range(idx + 1, 11):
-        dfs(i, total + idx)
+        dfs(i, total)
 
 
 result = 0
