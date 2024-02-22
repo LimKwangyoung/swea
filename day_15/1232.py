ba138c22d9b5a31d2a3751faeb07bb9c0aa93240
kokoz22 <kokoz22@naver.com>
2024-02-22 21:31:19 +0900

    day_15

--- day_15/1232.py
+++ day_15/1232.py
@@ -2,34 +2,33 @@ import sys
 
 sys.stdin = open(f'{__file__.split("/")[-1][:-3]}_input.txt')
 ##################################################
-import collections
+
 
 def calculate(node: int) -> float:
-    if tree[node] not in '+-*/':
-        return float(tree[node])
-    try:
-        left = calculate(2 * node)
-        right = calculate(2 * node + 1)
-    except:
-        print(node)
+    if values[node] not in '+-*/':
+        return float(values[node])
 
-    if tree[node] == '+':
-        tree[node] = left + right
-    elif tree[node] == '-':
-        tree[node] = left - right
-    elif tree[node] == '*':
-        tree[node] = left * right
+    left = calculate(tree[node][0])
+    right = calculate(tree[node][1])
+    if values[node] == '+':
+        values[node] = left + right
+    elif values[node] == '-':
+        values[node] = left - right
+    elif values[node] == '*':
+        values[node] = left * right
     else:
-        tree[node] = left / right
-    return tree[node]
+        values[node] = left / right
+    return values[node]
 
 
 for t in range(1, 11):
     N = int(input())
 
-    tree = collections.defaultdict(list)
+    tree = dict()
+    values = [None] * (N + 1)
     for _ in range(N):
-        num, val, *child = input().split()
-        tree[] = val
+        num, value, *child = input().split()
+        tree[int(num)] = list(map(int, child))
+        values[int(num)] = value
     calculate(1)
-    print(f'#{t} {int(tree[1])}')
+    print(f'#{t} {int(values[1])}')
