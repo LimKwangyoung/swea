7bf207e5e789e8ae14d9a4a154a29d1b9370b7ee
kokoz22 <kokoz22@naver.com>
2024-02-29 00:14:07 +0900

    day_20

--- day_20/1244.py
+++ day_20/1244.py
@@ -2,37 +2,47 @@ import sys
 
 sys.stdin = open(f'{__file__.split("/")[-1][:-3]}_input.txt')
 ##################################################
-
-
-def exchange(level: int) -> None:
-    global result
-
-    if level == cnt:
-        total = int(''.join(num))
-        result = max(result, total)
-        return
-
-    for i in range(N):
-        for j in range(N):
-            if i != j:
-                num[i], num[j] = num[j], num[i]
-                number = int(''.join(num))
-                if number not in used:
-                    used.add(number)
-                    exchange(level + 1)
-                # used.remove(int(''.join(num)))
-                    num[i], num[j] = num[j], num[i]
-
+import collections
 
 T = int(input())
 
 for t in range(1, T + 1):
-    num, cnt = map(int, input().split())
+    num, K = input().split()
 
-    used = {num}
-    arr = list(num)
-    N = len(arr)
+    arr = list(map(int, num))
+    K = int(K)
+    num_set = sorted(list(set(arr)))
 
-    result = 0
-    exchange(level=0)
-    print(f'#{t} {result}')
+    idx = 0
+    max_idx = -1
+    maximum = sorted(arr, reverse=True)
+    while K > 0:
+        if arr == maximum:
+            if K % 2:
+                index = collections.defaultdict(list)
+                for i, n in enumerate(arr):
+                    index[n].append(i)
+                for key, value in index.items():
+                    if len(value) >= 2:
+                        break
+                else:
+                    arr[-1], arr[-2] = arr[-2], arr[-1]
+            break
+        if arr[idx] >= num_set[max_idx]:
+            idx += 1
+            max_idx -= 1
+            continue
+
+        index = collections.defaultdict(list)
+        for i, n in enumerate([None] * idx + arr[idx:]):
+            index[n].append(i)
+
+        tmp = max(0, len(index[num_set[max_idx]]) - K)
+        change_idx = index[num_set[max_idx]][tmp]
+        arr[idx], arr[change_idx] = arr[change_idx], arr[idx]
+        idx += 1
+
+        if idx >= len(index[num_set[max_idx]]):
+            max_idx -= 1
+        K -= 1
+    print(f'#{t} {"".join(map(str, arr))}')
--- day_20/2871.py
+++ day_20/2871.py
@@ -4,6 +4,7 @@ sys.stdin = open(f'{__file__.split("/")[-1][:-3]}_input.txt')
 ##################################################
 
 
+# Solution 1
 def subsum(idx: int, path: list):
     global result
 
@@ -26,4 +27,39 @@ for t in range(1, T + 1):
 
     result = 0
     subsum(-1, [])
+    print(f'#{t} {result}')
+
+# Solution 2
+import collections
+import math
+
+
+def subsum(idx: int, cnt: int, total: int):
+    global result
+    if total >= K:
+        if total == K:
+            result += cnt
+        return
+
+    for i in range(idx, len(nums)):
+        num = nums[i]
+        for j in range(1, counts[num] + 1):
+            if total + num * j > K:
+                break
+            subsum(i + 1, cnt * (math.factorial(counts[num]) // (math.factorial(j) * math.factorial(counts[num] - j))), total + num * j)
+
+
+T = int(input())
+
+for t in range(1, T + 1):
+    N, K = map(int, input().split())
+    nums = list(map(int, input().split()))
+
+    counts = collections.defaultdict(int)
+    for n in nums:
+        counts[n] += 1
+    nums = list(counts.keys())
+
+    result = 0
+    subsum(0, 1, 0)
     print(f'#{t} {result}')
