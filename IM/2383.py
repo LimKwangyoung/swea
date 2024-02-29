e4160f655524b49b04af1b7bbaac1683e74541d9
LimKwangyoung <kokoz22@naver.com>
2024-02-29 17:39:52 +0900

    IM

---
+++ IM/2383.py
@@ -0,0 +1,65 @@
+import sys
+
+sys.stdin = open('input.txt')
+##################################################
+
+
+def line_up(person_arr: list, stair_info: tuple) -> int:
+    def cal_distance() -> int:
+        return abs(person[0] - stair[0]) + abs(person[1] - stair[1])
+
+    stair = stair_info[:2]
+    depth = stair[-1]
+
+    candidates = []
+    for person in person_arr:
+        candidates.append(cal_distance())
+    candidates.sort(reverse=True)
+
+    time = 0
+    stair = [None] * 3
+    while True:
+        if not candidates and stair.count(None) == 3:
+            return time
+
+        for idx in range(3):
+            if stair[idx]:
+                stair[idx] += 1
+                if stair[idx] == depth:
+                    if candidates[-1] <= time:
+                        stair[idx] = 0
+                        candidates.pop()
+                    else:
+                        stair[idx] = None
+        time += 1
+
+
+T = int(input())
+
+for t in range(1, T + 1):
+    N = int(input())
+
+    persons = []
+    stairs = []
+    for i in range(N):
+        line = list(map(int, input().split()))
+        for j in range(N):
+            if line[j] == 1:
+                persons.append((i, j))
+            elif line[j] > 1:
+                stairs.append((i, j, line[j]))
+
+    total_lst = []
+    n = len(persons)
+    for i in range(2 ** n):
+        tmp = []
+        for j in range(n):
+            if i & (1 << j):
+                tmp.append(persons[j])
+        total_lst.append(tmp)
+
+    result = 0
+    for lst_1 in total_lst:
+        lst_2 = list(set(persons) - set(lst_1))
+        result = min(result, max(line_up(lst_1, stairs[0]), line_up(lst_2, stairs[1])))
+    print(f'#{t} {result}')
--- IM/2477.py
+++ IM/2477.py
@@ -8,12 +8,63 @@ T = int(input())
 
 for t in range(1, T + 1):
     N, M, K, A, B = map(int, input().split())
-    receptions_time = list(map(int, input().split()))
-    repairs_time = list(map(int, input().split()))
-    clients = list(map(int, input().split()))
+    reception = list(map(int, input().split()))
+    repair = list(map(int, input().split()))
+    clients = collections.deque(list(map(int, input().split())))
 
-    receptions = receptions_time[:]
-    repairs = repairs_time[:]
-    time = clients[0]
+    result = 0
+
+    time = 0
+    client_id = 1
+    waiting = collections.deque([])
+    counter_1, counter_2 = [False] * N, [False] * M  # [process time, client id]
+    client_set = set()
     while True:
-        
+        if not clients and counter_1.count(False) == N and counter_2.count(False) == M and not waiting:
+            break
+
+        # repair
+        for i, counter in enumerate(counter_2):
+            if counter:
+                counter_2[i][0] -= 1  # repair
+                if counter_2[i][0] == 0:  # complete repair
+                    if waiting:  # enter for repair
+                        counter_2[i] = [repair[i], waiting.popleft()]
+                        if i + 1 == B and counter_2[i][1] in client_set:  # target counter in repair
+                            result += counter_2[i][1]
+                    else:  # initializing
+                        counter_2[i] = False
+            elif waiting:  # enter for repair
+                counter_2[i] = [repair[i], waiting.popleft()]
+                if i + 1 == B and counter_2[i][1] in client_set:  # target counter in repair
+                    result += counter_2[i][1]
+
+        # reception
+        for i, counter in enumerate(counter_1):
+            if counter:
+                counter_1[i][0] -= 1  # reception
+                if counter_1[i][0] == 0:  # complete reception
+                    waiting.append(counter[1])
+                    if clients and time >= clients[0]:  # enter for reception
+                        counter_1[i] = [reception[i], client_id]
+                        clients.popleft()
+                        client_id += 1
+                        if i + 1 == A:  # target counter in reception
+                            client_set.add(counter_1[i][1])
+                    else:
+                        counter_1[i] = False
+            elif clients and time >= clients[0]:  # enter for reception
+                counter_1[i] = [reception[i], client_id]
+                clients.popleft()
+                client_id += 1
+                if i + 1 == A:  # target counter in reception
+                    client_set.add(counter_1[i][1])
+
+        # client for waiting
+        for i, counter in enumerate(counter_2):
+            if waiting and not counter:
+                counter_2[i] = [repair[i], waiting.popleft()]
+                if i + 1 == B and counter_2[i][1] in client_set:  # target counter in repair
+                    result += counter_2[i][1]
+        time += 1
+    print(f'#{t} {result if result else -1}')
