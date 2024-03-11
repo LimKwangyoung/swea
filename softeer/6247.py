b0dc7456978ba793cb0abaa76663ecf7f3115c19
kokoz22 <kokoz22@naver.com>
2024-03-12 00:10:44 +0900

    softeer

--- softeer/6247.py
+++ softeer/6247.py
@@ -1,8 +1,9 @@
+import sys
 import collections
 
 # Solution 1
-n, q = map(int, input().split())
-nums = sorted(list(map(int ,input().split())))
+n, q = map(int, sys.stdin.readline().split())
+nums = sorted(list(map(int, sys.stdin.readline().split())))
 expectations = [int(input()) for _ in range(q)]
 
 counts = collections.defaultdict(int)
@@ -14,8 +15,8 @@ for num in expectations:
     print(counts[num] * (n - 1 - counts[num]))
 
 # Solution 2
-n, q = map(int, input().split())
-nums = sorted(list(map(int ,input().split())))
+n, q = map(int, sys.stdin.readline().split())
+nums = sorted(list(map(int, sys.stdin.readline().split())))
 
 for _ in range(q):
     num = int(input())
@@ -23,11 +24,12 @@ for _ in range(q):
     left, right = 0, n - 1
     while left <= right:
         mid = (left + right) // 2
+        if nums[mid] == num:
+            print(mid * (n - mid - 1))
+            break
         if nums[mid] < num:
             left = mid + 1
         else:
             right = mid - 1
-    if left < n and nums[left] == num:
-        print(left * (n - left - 1))
     else:
         print(0)
