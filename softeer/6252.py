d387157ad24ed2e829178f16f8ea62ff07244efb
LimKwangyoung <kokoz22@naver.com>
2024-03-18 17:44:39 +0900

    softeer

--- softeer/6252.py
+++ softeer/6252.py
@@ -3,13 +3,13 @@ import collections
 
 
 def cal_cost(cost: int) -> int:
-    return sum(((cost - power) ** 2) * powers[power] for power in powers if cost > power)
+        return sum(((cost - power) ** 2) * powers[power] for power in powers if cost > power)
 
 
 def binary_search(left: int, right: int) -> int:
     while left <= right:
         mid = (left + right) // 2
-        if cal_cost(mid) < B:
+        if cal_cost(mid) <= B:
             left = mid + 1
         else:
             right = mid - 1
@@ -22,4 +22,4 @@ powers = collections.defaultdict(int)
 for i in map(int, sys.stdin.readline().split()):
     powers[i] += 1
 
-print(binary_search(1, 2 * B))
+print(binary_search(1, 2 * 10 ** 9))
